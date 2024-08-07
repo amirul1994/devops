7844151254534e46b5052b79ce1750f67e43df93
Amirul1994 <119165587+Amirul1994@users.noreply.github.com>
2024-08-07 19:59:06 +0600

    Update app.js

--- jenkins/todo-list-node/app.js
+++ jenkins/todo-list-node/app.js
@@ -36,6 +36,6 @@ app.post('/delete', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => { // Listen on all network interfaces
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });

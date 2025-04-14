a9917f7b82b0786adac47c2df161dc299db5e932
amirul1994 <amirulbrinto90@gmail.com>
2025-04-14 16:41:18 +0600

    added ansibl playbook for kubernetes nodes and namespaces info

---
+++ kubedeploy_webapp_cicd_iac/ansible/test.py
@@ -0,0 +1,8 @@
+namesapces = {
+    'all-namespaces': ['default', 'kube-node-lease', 'kube-public', 'kube-system']
+}
+
+# all_namespaces = namesapces.values()
+all_namespaces = namesapces["all-namespaces"]
+
+print(all_namespaces)
